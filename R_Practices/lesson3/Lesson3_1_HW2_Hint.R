7b51309d477b3aea253135269ebf92e034df6dcd
Fettah Kiran <37385211+ftth05@users.noreply.github.com>
2024-02-02 15:46:49 -0600

    Lecture 3 Practices

---
+++ R_Practices/lesson3/Lesson3_1_HW2_Hint.R
@@ -0,0 +1,402 @@
+##
+# Lecture 3
+## HW2 Hints
+
+
+library(tidyverse)
+library(dplyr)
+library(lubridate)
+library(ggplot2)
+library(cowplot)
+library(ggpubr)
+library(lme4)
+library(readr)
+library(lmerTest)
+library(lattice)
+library(gridExtra)
+
+
+# dataDir <- paste0("../../Data/")
+# file = "Multimodal-Activity-Dataset.csv"
+# 
+# 
+# all.df = read.csv(paste0(dataDir, file), stringsAsFactors = F)
+# str(all.df)
+# 
+# all.df %>%
+#   dplyr::select(Time,Activity, HR, Cadence, Activity) %>%
+#   filter(Activity == c("walking", "running"))  -> all.df
+# # 
+# write.csv(all.df, paste0("Multimodal-Activity-Dataset2.csv"), row.names = F)
+
+file = "Lecture3_Dataset.csv"
+
+all.df = read.csv(paste0(file), stringsAsFactors = F)
+str(all.df)
+
+
+summary(all.df)
+
+nlevels(all.df$Activity)
+unique(all.df$Activity)
+all.df$Activity = factor(all.df$Activity, levels = c( "walking", "running"))
+nlevels(all.df$Activity)
+unique(all.df$Activity)
+
+# find the NA rows in HR
+
+all.df.HR.NAs = all.df[is.na(all.df$HR),]
+nrow(all.df.HR.NAs)
+
+# remove the NA rows in HR
+all.df = all.df[!is.na(all.df$HR),]
+all.df= all.df %>% group_by(Activity)
+
+str(all.df)
+
+#density functions
+
+# - Filters
+# 
+# CH1 (HR) 		CH2(Cadence)   Product
+# Entity A (Wlk)
+# 
+# Entity B (Rnng)
+
+
+
+
+# - Density functions
+?density
+tmp.d <- density(all.df$HR)
+plot(tmp.d, main = "HR Density", col = "red")
+
+# create a function / filter
+?approxfun
+# plot the function
+tmm.p.d_fun1 <- approxfun(tmp.d$x, tmp.d$y)
+
+
+
+tmm.p.d_fun1(100)
+tmm.p.d_fun1(120)
+tmm.p.d_fun1(140)
+
+
+
+# - Density functions
+?density
+tmp.d1 <- density(all.df[all.df$Activity == "walking",]$HR)
+plot(tmp.d1, main = "Walking HR Density")
+
+# create a function / filter
+?approxfun
+# plot the function
+tmm.p.d_fun1 <- approxfun(tmp.d1$x, tmp.d1$y)
+points(tmp.d1$x, tmp.d1$y, col = "red")
+
+
+
+tmp.d2 <- density(all.df[all.df$Activity == "walking",]$Cadence)
+plot(tmp.d2, main = "Walking Cadence Density")
+# create a function / filter
+tmm.p.d_fun2 <- approxfun(tmp.d2$x, tmp.d2$y)
+
+
+tmp.d3 <- density(all.df[all.df$Activity == "running",]$HR)
+plot(tmp.d3, main = "Running HR Density")
+# create a function / filter
+tmm.p.d_fun3 <- approxfun(tmp.d2$x, tmp.d2$y)
+
+tmp.d4 <- density(all.df[all.df$Activity == "running",]$Cadence)
+plot(tmp.d4, main = "Running Cadence Density")
+# create a function / filter
+tmm.p.d_fun4 <- approxfun(tmp.d4$x, tmp.d4$y)
+
+
+
+
+
+
+all.df %>%
+  mutate(walk_hr_density = tmm.p.d_fun1(HR),
+         walk_cadence_density = tmm.p.d_fun2(Cadence),
+         walk_multi = walk_hr_density * walk_cadence_density,
+         run_hr_density = tmm.p.d_fun3(HR),
+         run_cadence_density = tmm.p.d_fun4(Cadence),
+         run_multi = run_hr_density * run_cadence_density 
+         ) -> all.df
+
+
+colnames(all.df)
+
+# Ensure that Time is in POSIXct format
+all.df$Time <- as.POSIXct(all.df$Time, format = "%H:%M:%S")
+
+
+
+
+# With walking data ----
+
+tmp_activity = "walking"
+tmp.min = 0
+tmp.max = 0.3
+
+
+P11<-all.df %>%
+  filter(Activity == tmp_activity) %>%
+  ggplot() +
+  geom_line( aes(x=Time, y = walk_hr_density) )   +
+  theme_bw() +ylim(tmp.min,tmp.max) +
+  labs(x = "", y = "") + ggtitle("Walking HR Filter")
+
+P11
+
+P12 <- all.df %>%
+  filter(Activity == tmp_activity) %>%
+  ggplot() +
+  geom_line( aes(x=Time, y = walk_cadence_density) )+
+  theme_bw() +ylim(tmp.min,tmp.max) +
+  labs(x =  "", y = "") + ggtitle("Walk Cadence Filter")
+
+P12
+P13<-all.df %>%
+  filter(Activity == tmp_activity) %>%
+  ggplot() +
+  # geom_rect(aes(xmin = Time2,
+  #               xmax = dplyr::lead(Time2),
+  #               ymin = 0, ymax = Inf,
+  #               fill = winner1), alpha = 1) +
+  geom_line( aes(x=Time, y = walk_multi) ) +
+  theme_bw() +ylim(tmp.min,tmp.max) +
+  labs(x =  "", y = "") + ggtitle("Combine Walking  Filter")
+
+P13
+
+
+# walking
+tmp_activity = "walking"
+
+P21<-all.df %>%
+  filter(Activity == tmp_activity) %>%
+  ggplot() +
+  geom_line( aes(x=Time, y = run_hr_density) )  +
+  theme_bw() +ylim(tmp.min,tmp.max) +
+  labs(x = "", y = "") + ggtitle("Running HR Filter")
+
+P21
+
+P22<-all.df %>%
+  filter(Activity == tmp_activity) %>%
+  ggplot() +
+  geom_line( aes(x=Time, y = run_cadence_density) ) +
+  theme_bw() +ylim(tmp.min,tmp.max) +
+  labs(x =  "", y = "") + ggtitle("Running Cadence Filter")
+
+P22
+P23<-all.df %>%
+  filter(Activity == tmp_activity) %>%
+  ggplot() +
+  # geom_rect(aes(xmin = Time2,
+  #               xmax = dplyr::lead(Time2),
+  #               ymin = 0, ymax = Inf,
+  #               fill = winner1), alpha = 1) +
+  geom_line( aes(x=Time, y = run_multi) ) +
+  theme_bw() +ylim(tmp.min,tmp.max) +
+  labs(x =  "", y = "") + ggtitle("Combine Running Filter")
+
+P23
+
+
+Walking_final <- ggarrange(P11, P12, P13, P21, P22, P23,ncol = 3, nrow = 2, common.legend = TRUE, legend = "bottom") +
+  ggtitle("Probabilistic filtering  on Walking") +
+  theme(plot.title = element_text(hjust = 0.5)) +
+  theme(legend.position = "none")
+
+# With running data ----
+  
+tmp_activity = "running"
+tmp.min = 0
+tmp.max = 0.3
+
+
+P11<-all.df %>%
+  filter(Activity == tmp_activity) %>%
+  ggplot() +
+  geom_line( aes(x=Time, y = walk_hr_density) )   +
+  theme_bw() +ylim(tmp.min,tmp.max) +
+  labs(x = "", y = "") + ggtitle("Walk HR Filter")
+
+P11
+
+P12<-all.df %>%
+  filter(Activity == tmp_activity) %>%
+  ggplot() +
+  geom_line( aes(x=Time, y = walk_cadence_density) )+
+  theme_bw() +ylim(tmp.min,tmp.max) +
+  labs(x =  "", y = "") + ggtitle("Walk Cadence Filter")
+
+P12
+P13<-all.df %>%
+  filter(Activity == tmp_activity) %>%
+  ggplot() +
+  # geom_rect(aes(xmin = Time2,
+  #               xmax = dplyr::lead(Time2),
+  #               ymin = 0, ymax = Inf,
+  #               fill = winner1), alpha = 1) +
+  geom_line( aes(x=Time, y = walk_multi) ) +
+  theme_bw() +ylim(tmp.min,0.005) +
+  labs(x =  "", y = "") + ggtitle("Combine Walking Filter")
+
+P13
+
+
+
+P21<-all.df %>%
+  filter(Activity == tmp_activity) %>%
+  ggplot() +
+  geom_line( aes(x=Time, y = run_hr_density) )  +
+  theme_bw() +ylim(tmp.min,tmp.max) +
+  labs(x = "", y = "") + ggtitle("Running HR Filter")
+
+P21
+
+P22<-all.df %>%
+  filter(Activity == tmp_activity) %>%
+  ggplot() +
+  geom_line( aes(x=Time, y = run_cadence_density) ) +
+  theme_bw() +ylim(tmp.min,tmp.max) +
+  labs(x =  "", y = "") + ggtitle("Running Cadence Filter")
+
+P22
+P23<-all.df %>%
+  filter(Activity == tmp_activity) %>%
+  ggplot() +
+  # geom_rect(aes(xmin = Time2,
+  #               xmax = dplyr::lead(Time2),
+  #               ymin = 0, ymax = Inf,
+  #               fill = winner1), alpha = 1) +
+  geom_line( aes(x=Time, y = run_multi) ) +
+  theme_bw() + ylim(tmp.min,0.005) +
+  labs(x =  "", y = "") + ggtitle("Combine Running  Filter")
+
+P23
+
+
+Running_final <- ggarrange(P11, P12, P13, P21, P22, P23,ncol = 3, nrow = 2, common.legend = TRUE, legend = "bottom") +
+  ggtitle("Probabilistic filtering on Running Data") +
+  theme(plot.title = element_text(hjust = 0.5)) +
+  theme(legend.position = "none")
+
+
+
+
+
+
+
+
+
+Final_final <- ggarrange(Walking_final, Running_final, ncol = 1, nrow = 2, common.legend = TRUE, legend = "bottom") +
+  theme(plot.title = element_text(hjust = 0.5)) +
+  theme(legend.position = "none")
+
+
+ggsave("Final_final.pdf", plot = Final_final, width = 10, height = 10, units = "in", dpi = 300)
+
+
+
+
+
+
+------------
+
+# find the max label for each activity in product and save it's index to color background
+
+mycolors <- c(winner = "red", loser = "white")
+
+all.df %>% 
+ mutate(max_ind = pmap_int(list(walk_multi, run_multi), ~which.max(c(...)))) 
+all.df
+
+pmap_int(list(all.df$walk_multi, all.df$run_multi), ~which.max(c(...)))
+
+sum(is.na(all.df$walk_multi))
+sum(is.na(all.df$run_multi))
+
+
+
+
+
+
+
+#3D visualization
+library(plotly)
+
+
+# 2 D
+all.df %>%
+  #filter(Activity == tmp_activity) %>%
+  plot_ly(x = ~HR ~Cadence    ,
+          color = ~Activity, colors = c("gray", "green", "brown", "orange", "cyan"),
+          #type = "mesh3d") %>%
+  #type = "scatter3d",
+  mode = "markers") %>%
+  layout(title = paste0("3D Visualization of Activities"),
+         scene = list(xaxis = list(title = "HR [BPM]"),
+                      yaxis = list(title = "Speed [km/h]"),
+                      zaxis = list(title = "Cadence [RPM]")) )
+
+
+# 2 Dimensional Visualization  on time
+
+
+all.df %>%
+  #filter(Activity == tmp_activity) %>%
+  plot_ly(x = ~Time  , y = ~HR  ,
+          color = ~Activity, colors = c("gray", "green", "brown", "orange", "cyan"),
+          #type = "mesh3d") %>%
+          #type = "scatter3d",
+          mode = "markers") %>%
+  layout(title = paste0("3D Visualization of Activities"),
+         scene = list(xaxis = list(title = "Time"),
+                      yaxis = list(title = "Speed [km/h]"),
+                      zaxis = list(title = "Cadence [RPM]")) )
+
+all.df %>%
+  #filter(Activity == tmp_activity) %>%
+  plot_ly(x = ~Time  , y = ~Cadence  ,
+          color = ~Activity, colors = c("gray", "green", "brown", "orange", "cyan"),
+          #type = "mesh3d") %>%
+          #type = "scatter3d",
+          mode = "markers") %>%
+  layout(title = paste0("3D Visualization of Activities"),
+         scene = list(xaxis = list(title = "Time"),
+                      yaxis = list(title = "Speed [km/h]"),
+                      zaxis = list(title = "Cadence [RPM]")) )
+
+
+
+
+# 3D visualization
+
+tmp_activity = "walking"
+all.df %>%
+  #filter(Activity == tmp_activity) %>%
+  plot_ly(x = ~HR, y = ~Cadence, 
+          color = ~Activity, colors = c("gray", "green", "brown", "orange", "cyan"),
+  #        type = "mesh3d") %>%
+  type = "scatter",mode = "markers") %>%
+  layout(title = paste0("3D Visualization of Activities"),
+         scene = list(xaxis = list(title = "HR [BPM]"),
+                      yaxis = list(title = "Speed [km/h]"),
+                      zaxis = list(title = "Cadence [RPM]")) )
+
+
+
+
+
+
+
+
+
+
---
+++ R_Practices/lesson3/Lesson3_2_Inference.R
@@ -0,0 +1,311 @@
+# Sources
+# https://www.r-bloggers.com/2021/06/how-to-find-z-score-in-r-easy-calculation-quick-guide/
+# https://bookdown.org/mrenna/statbook/statistical-inference.html#hypothesis-testing
+# https://sphweb.bumc.bu.edu/otlt/MPH-Modules/BS/R/R-Manual/R-Manual10.html
+
+# Plan:
+# 1. Sampling distribution
+# 2. Sampling size and Sampling distribution
+# 3. Z score
+# 4. Hypothesis testing
+
+
+# SAMPLING DISTRIBUTION
+library(dplyr)
+ames <- read.csv("http://bit.ly/315N5R5") 
+dplyr::glimpse(ames) # you need dplyr to use this function
+
+area <- ames$Gr.Liv.Area
+price <- ames$SalePrice
+
+head(area, n=10) # show first 10 observations 
+head(price, n=10) # show first 10 observations 
+
+length(area) # how many observations in the vector?
+
+any(is.na(area)) #is there any NA in the vector area?
+sd(area) # Population standard deviation
+
+summary(area) # Population mean
+summary(area)[[4]] # Population mean
+
+hist(area,
+     main = bquote(paste("Above ground living area (sq.ft.) |", ~italic("n"),  " = ", 
+                         .(length(area)), ", ", ~mu, " = ", .(round(mean(area), 2)), ", ",
+                         ~sigma, " = ", .(round(sd(area), 2))),
+                         )
+     )
+     
+
+
+
+area <- ames$Gr.Liv.Area # create new dataset containing only variable 'Gr.Liv.Area' from dataset 'ames'
+samp1 <- sample(area, 10) # take a random sample of 10 observations from the dataset 'area'
+mean(samp1) # mean of the sample distribution for area. Note difference from population mean.
+
+
+# Exercise (sampling, comparing means)
+# 1. make two samples n1 = 50, n2 = 1000
+# compare means
+
+area <- ames$Gr.Liv.Area # create new dataset containing only variable 'Gr.Liv.Area' from dataset 'ames'
+samp1 <- sample(area, 50) # take a random sample of 50 observations from the dataset 'area'
+mean(samp1) #
+
+samp2 <- sample(area, 1000) # take a random sample of 1000 observations from the dataset 'area'
+mean(samp2) #
+
+
+# 2. suppose we have samples n3 = 1500, n4 = 2000. 
+# which is more accurate?
+
+
+
+area <- ames$Gr.Liv.Area 
+sample_means50 <- rep(NA, 500) # initialise a vector
+
+for (i in 1:500) { # use of a loop function to draw a random sample 5000 times
+    samp <- sample(area, 50)
+    sample_means50[i] <- mean(samp)
+}
+
+hist(sample_means50, breaks = 25, 
+     main = bquote(paste("sample_means50 living area (sq.ft.) |", ~italic("n"),  " = ", 
+                         .(length(sample_means50)), ", ", ~mu, " = ", .(round(mean(sample_means50), 2)), ", ",
+                         ~sigma, " = ", .(round(sd(sample_means50), 2))),
+     ),
+     
+     xlab = "Means (sq.ft.)") #Histogram of the 500 samples (sampling distribution of the samples mean)
+
+
+
+
+
+# SAMPLING SIZE AND DISTRIBUTION
+# To get a sense of the effect that sample size has on 
+# our distribution, let’s build up two more sampling 
+# distributions: one based on a sample size of 10 and 
+# another based on a sample size of 100 from a 
+# population size of 5000.
+
+
+# Lets plot data to see the differences
+
+
+area <- ames$Gr.Liv.Area 
+sample_means10 <- rep(NA, 5000)
+sample_means50 <- rep(NA, 5000)
+sample_means100 <- rep(NA, 5000)
+
+for(i in 1:5000){
+    samp <- sample(area, 10)
+    sample_means10[i] <- mean(samp)
+    samp <- sample(area, 50)
+    sample_means50[i] <- mean(samp)
+    samp <- sample(area, 100)
+    sample_means100[i] <- mean(samp)
+}
+
+par(mfrow = c(3, 1))  # this creates 3 rows and 1 column for graphs
+
+xlimits <- range(sample_means10)
+
+hist(sample_means10,  breaks = 25, xlim = xlimits)
+hist(sample_means50,  breaks = 25, xlim = xlimits)
+hist(sample_means100, breaks = 25, xlim = xlimits)
+
+par(mfrow = c(1, 1))  
+
+
+# HYPOTHESIS TESTING
+# Z-score
+
+# How to find? 
+# Z-score provides how many standard deviations away a value 
+# is from the mean.
+
+# We can use the following formula for z-score calculation.
+# Z-score = (x-μ)/σ
+# x is a raw score to be standardized;
+# μ is the mean of the population;
+# σ is the standard deviation of the population.
+
+# Example 1, single vector
+data <- c(8, 7, 7, 10, 13, 14, 15, 16, 18) 
+z_scores <- (data-mean(data))/sd(data)
+z_scores
+
+# The first raw data value of “8” is 0.9701425 standard deviations below the mean.
+# The second raw data value of “7” is -1.2126781 standard deviations below the mean.
+# The ninth raw data value of “18” is 0.2425356 standard deviations above the mean.
+
+dev.off()
+plot(z_scores, type="o", col="red")
+
+# what if we will calculate z-scores for the standard normal distribution?
+data_norm <- rnorm(100, mean = 0, sd = 1)
+z_scores_norm <- (data_norm-mean(data_norm))/sd(data_norm)
+plot(z_scores_norm, type="o", col="red")
+
+# how can we get a better picture?
+plot(sort(z_scores_norm), type="o", col="red")
+plot(x = data_norm, y = z_scores_norm)
+
+# Example 2, Single Column in a DataFrame
+data<- data.frame(A = c(2, 5, 6, 7, 9, 13),
+                  B = c(24, 20, 13, 15, 19, 20),
+                  C = c(15, 5, 7, 18, 14, 10))
+z_scores <- (data$B-mean(data$B))/sd(data$B)
+z_scores
+
+# The first raw data value of “24” is 1.3970014 standard deviations 
+# above the mean.
+# The first raw data value of “20” is 0.3810004  standard deviations 
+# above the mean.
+
+
+# In general, the z score tells you how far a value is 
+# from the average of the data in terms of standard deviations.
+
+# qnorm in r - function to calculate z score in r
+qnorm(0.75, mean = 0, sd = 1)
+# Its value being below 1 means that the point that separates the 
+# lower 75% observations and upper 25% observations is within one 
+# standard deviation of the average, towards the right.
+
+
+# The standard normal (z) distribution
+
+# The pnorm( ) function gives the area, or probability, 
+# below a z-value:
+pnorm(1.96)
+
+# To find a two-tailed area 
+# (corresponding to a 2-tailed p-value) for a positive z-value:
+2*(1-pnorm(1.96))
+
+# The qnorm( ) function gives critical z-values corresponding 
+# to a given lower-tailed area:
+qnorm(.05)
+
+# To find a critical value for a two-tailed 95% confidence interval:
+qnorm(1-.05/2)
+
+
+# HYPOTHESIS TESTING
+
+
+
+
+# https://statstutorial.com/how-to-perform-a-one-sample-z-test-in-r-with-examples/
+
+# Z-Test in Statistics with Examples ----
+
+# Example 1: One-sample Z-test
+# Null Hypothesis: The mean of a population is equal to a specified value.
+# Alternative Hypothesis: The mean of a population is not equal to a specified value.
+
+# Perform one-sample Z-test
+#install.packages("BSDA")
+
+# z.test(x,y = NULL,
+#        alternative = "two.sided"or"greater", "less" or
+#        mu = 0,
+#        sigma.x = NULL,
+#        sigma.y = NULL,
+#        conf.level = 0.95
+# )
+
+
+library(BSDA)
+# Define given dataset
+dataset <- c(670,730,540,670,480,800,690,560,590,620,700,660,640,710,650,490,800
+             ,600,560,700,680,550,580,700,705,690,520,650,660,790)
+
+#Create qqplot for the dataset
+
+qqnorm(dataset)
+qqline(dataset)
+
+# Step 1: Define the Null Hypothesis and Alternate Hypothesis.
+# 
+# let μ be the mean weight of almonds
+# 
+# Null Hypothesis: the mean score is equal to 610
+# 
+# H0 : μ = 610
+# 
+# Alternate Hypothesis: the mean score is not equal to 610.
+# 
+# Ha : μ ≠  610
+# 
+# Step 2: level of significance (α) = 0.05
+# 
+# Step 3: Let’s check the assumptions.
+
+
+# When to use a z-Test: ----
+
+# - Your sample size must be greater than 30.
+# - Data points should be independent from each other. 
+#     In other words, one data point isn’t related or doesn’t affect another data point.
+# - Your data should be normally distributed.
+# - Samples should be drawn at random from the population.
+# - The standard deviation of the population should be known
+
+
+
+
+
+# Perform one-sample z-test ----
+
+z_test_result<- z.test(x=dataset,mu=610,alternative = "two.sided",sigma.x = 100)
+z_test_result
+
+# Print the results
+cat("\nExample 1: One-sample Z-test\n")
+cat("Test Statistic:", z_test_result$statistic, "\n")
+cat("P-value:", format(z_test_result$p.value,4), "\n")
+cat("Hypothesis Test Result:", ifelse(z_test_result$p.value < 0.05, "Reject Null Hypothesis", "Fail to Reject Null Hypothesis"), "\n\n")
+
+
+# Conclusion: ----
+#   
+# Since the p-value[0.0476] is less than the level of significance (α) = 0.05, we reject the null hypothesis.
+# 
+# This means we have sufficient evidence to say that the mean score for the students is not equal to 610.
+
+
+# Two Sample z test: 
+##The Two-Sample Z-test is used to compare the means of two samples 
+##to see if it is feasible that they come from the same population.
+
+
+
+# Example 2: Two-sample Z-test
+# Null Hypothesis: The means of two populations are equal.
+# Alternative Hypothesis: The means of two populations are not equal.
+
+
+
+# Perform two-sample Z-test
+
+# Null Hypothesis (H0): The means of two populations are equal.
+# Alternative Hypothesis (H1): The means of two populations are not equal.
+
+set.seed(456)
+group1 <- rnorm(25, mean = 35, sd = 6)  # Group 1 data with mean 35
+group2 <- rnorm(30, mean = 40, sd = 6)  # Group 2 data with mean 40
+
+qqnorm(group1)
+qqnorm(group2)
+
+# Assuming known population standard deviations for illustration
+z_test_result <- z.test(group1, group2, alternative = "two.sided", sigma.x = 6, sigma.y = 6)
+z_test_result
+
+# Print the results
+cat("\nExample 2: Two-sample Z-test\n")
+cat("Test Statistic:", z_test_result$statistic, "\n")
+cat("P-value:", format(z_test_result$p.value,4), "\n")
+cat("Hypothesis Test Result:", ifelse(z_test_result$p.value < 0.05, "Reject Null Hypothesis", "Fail to Reject Null Hypothesis"), "\n\n")
